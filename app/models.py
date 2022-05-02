eee7988b02d987de5f5d1760822c9de33e833303
maureenndiema <ndiemam@gmail.com>
2022-05-02 14:17:16 +0300

    function to define objects on class sources

--- app/models.py
+++ app/models.py
@@ -30,3 +30,15 @@ class Keyword:
     self.imageurl =imageurl
     self.publishedAt = publishedAt
     self.url = url
+
+class Sources:
+    '''
+    Sources class to define Sources Objects
+    '''
+    def __init__(self,id,name,description,url,category,language):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
