1a3966651d90274e9147de464dd1f6c3107c648f
maureenndiema <ndiemam@gmail.com>
2022-05-02 14:16:40 +0300

    function to define objects on class keyword

--- app/models.py
+++ app/models.py
@@ -18,3 +18,15 @@ class Breaking:
     self.title = title
     self.imageurl =imageurl
     self.url = url
+
+class Keyword:
+  '''
+  Articles class to define Article objects
+  '''
+
+  def __init__(self, author, title, imageurl, publishedAt, url):
+    self.author = author
+    self.title = title
+    self.imageurl =imageurl
+    self.publishedAt = publishedAt
+    self.url = url
