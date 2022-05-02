9fc2b20b047e578e57a9623f9014f1582ddc0360
maureenndiema <ndiemam@gmail.com>
2022-05-02 14:20:44 +0300

    initializing flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,6 @@ def create_app(config_name):
 
      # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app
