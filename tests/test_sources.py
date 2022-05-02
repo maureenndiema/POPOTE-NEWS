e6b612c96861128ac6a65b0395a823a9ce4c0a12
maureenndiema <ndiemam@gmail.com>
2022-05-02 15:08:31 +0300

    Test case to test if the object is an instance of the class

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,2 +1,19 @@
 import unittest
 from app.models import Sources
+
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Sources class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Sources(1234, 'ABC News', 'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.', 'https://abcnews.go.com', 'general', 'en')
+
+    def test_instance(self):
+        '''
+        Test case to test if the object is an instance of the class
+        '''
+        self.assertTrue(isinstance(self.new_source, Sources))
