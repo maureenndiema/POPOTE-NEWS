05a0bd7e0dc55da3a671f84f73284cbbd4310df2
maureenndiema <ndiemam@gmail.com>
2022-05-02 13:50:24 +0300

    add news api key & secret key

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
 import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL = 'http://newsapi.org/v2/sources?&apiKey={}'
+    NEWS_API_TOP_ARTICLES_BASE_URL = 'http://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_SEARCH_KEYWORD_BASE_URL = 'https://newsapi.org/v2/everything?q={}&sortBy=relevancy,publishedAt&pageSize=30&apiKey={}'
+    NEWS_API_BREAKING_NEWS_BASE_URL = 'http://newsapi.org/v2/top-headlines?country=us&category=general&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+
