22509176d7066c310c33a0e9b2521ecec74bc2cd
maureenndiema <ndiemam@gmail.com>
2022-05-02 13:58:10 +0300

    import urllb

---
+++ app/requests.py
@@ -0,0 +1,2 @@
+import urllib.request,json #helps create a connection to our API URL and send a request, formats the JSON response to a Python dictionary
+from .models import Sources,Articles,Keyword,Breaking
