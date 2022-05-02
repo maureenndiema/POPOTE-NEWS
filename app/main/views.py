77ad68cadf442ead8e90bef1b82034babb949312
maureenndiema <ndiemam@gmail.com>
2022-05-02 14:56:17 +0300

    function to display search results

--- app/main/views.py
+++ app/main/views.py
@@ -18,3 +18,29 @@ def index():
         return redirect(url_for('main.keyword', keyword_name=get_keyword))
     else:
         return render_template('index.html', title = title, sources=sources, breaking_news=breaking_news)
+
+@main.route('/source/<sourcesId>')
+def articles(sourcesId):
+    '''
+    View function that displays top stories from a particular source
+    '''
+    articles= get_top_articles(sourcesId)
+    title = f"{sourcesId}"
+    header = sourcesId.upper()
+    get_keyword = request.args.get('keyword_query')
+
+    if get_keyword:
+        return redirect(url_for('main.keyword', keyword_name=get_keyword))
+    else:
+        return render_template('articles.html', title=title, header=header, article=articles)
+
+@main.route('/search/<keyword_name>')
+def keyword(keyword_name):
+    '''
+    View function to display the search results
+    '''
+    keyword_name_list = keyword_name.split(" ")
+    keyword_name_format = "+".join(keyword_name_list)
+    searched_keyword = get_keyword(keyword_name_format)
+    title = f'{keyword_name.title()}'
+    display_keyword = keyword_name.upper()
