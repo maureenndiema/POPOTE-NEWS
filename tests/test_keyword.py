79cf18dc18871b806b2b1dc7dd4e56ca9d98cdc9
maureenndiema <ndiemam@gmail.com>
2022-05-02 15:06:23 +0300

    est case to test if the object is an instance of the class

--- tests/test_keyword.py
+++ tests/test_keyword.py
@@ -17,3 +17,13 @@ class KeywordTest(unittest.TestCase):
         Test case to test if the object is an instance of the class
         '''
         self.assertTrue(isinstance(self.new_keyword, Keyword))
+
+    def test_initialization(self):
+        '''
+        Test initialization test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_keyword.author, 'Stan Schroeder' )
+        self.assertEqual(self.new_keyword.imageurl, 'https://abcnews.go.com' )
+
+if __name__ == '__main__':
+    unittest.main()
