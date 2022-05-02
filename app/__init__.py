a20ecd8e36c8132939fa1e2e23c5895234f74a21
maureenndiema <ndiemam@gmail.com>
2022-05-02 14:22:43 +0300

    setting config

--- app/__init__.py
+++ app/__init__.py
@@ -18,3 +18,12 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
+
+    
+
