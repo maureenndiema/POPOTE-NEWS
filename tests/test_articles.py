0949618b2d6bd3dd07fa9ed9803e237b6b063aca
maureenndiema <ndiemam@gmail.com>
2022-05-02 14:59:28 +0300

    setting up method that will run before every test

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -1,2 +1,19 @@
 import unittest
 from app.models import Articles
+
+class ArticlesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Articles class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Articles('Stan Schroeder', 'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.', 'https://abcnews.go.com', '2020-05-16T00:34:00Z', 'https://mashable.com/article/bitcoin-halving-2020/')
+
+    def test_instance(self):
+        '''
+        Test case to test if the object is an instance of the class
+        '''
+        self.assertTrue(isinstance(self.new_article, Articles))
